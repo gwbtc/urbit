gwbtc/urbit

commit 62c753f7a83d835da8e919014c12475d68558afa

chat-cli: reconnect to chat-store on-kick
Also reconnects on-load, if no chat-store subscription appears to be
open. This should resolve any current disconnects.

pkg/arvo/app/chat-cli.hoon
@@ -122,7 +122,7 @@
       ?-    -.sign
           %poke-ack   [- all-state]:(on-agent:def wire sign)
           %watch-ack  [- all-state]:(on-agent:def wire sign)
-          %kick       ~&  %chat-cli-kicked  `all-state
+          %kick       [?:(?=([%chat-store ~] wire) ~[connect] ~) all-state]
           %fact
         ?+  p.cage.sign  ~|([%chat-cli-bad-sub-mark wire p.cage.sign] !!)
           %chat-update  (diff-chat-update:tc wire !<(chat-update q.cage.sign))
@@ -141,7 +141,10 @@
   |=  old=(unit state)
   ^-  (quip card state)
   ?^  old
-    [~ u.old]
+    :_  u.old
+    ?:  (~(has by wex.bowl) [/chat-store our-self %chat-store])
+      ~
+    ~[connect]
   =^  cards  all-state
     %_  catch-up
       audience  [[our-self /] ~ ~]

commit 3d7970874db5b369423c81d963cd15818e387d95

chat-cli: reconnect with actual bowl
Fixes a bug introduced in 62c753f.

This, uh, fell into the same old case of using an arm from a |_ without
initializing that core with a sample first. In this case, that resulted
in the bowl in connect being the default bowl here. This is fine for
~zod, since it's the default ship, but gives incorrect behavior for
anyone else.

pkg/arvo/app/chat-cli.hoon
@@ -122,7 +122,7 @@
       ?-    -.sign
           %poke-ack   [- all-state]:(on-agent:def wire sign)
           %watch-ack  [- all-state]:(on-agent:def wire sign)
-          %kick       [?:(?=([%chat-store ~] wire) ~[connect] ~) all-state]
+          %kick       [?:(?=([%chat-store ~] wire) ~[connect:tc] ~) all-state]
           %fact
         ?+  p.cage.sign  ~|([%chat-cli-bad-sub-mark wire p.cage.sign] !!)
           %chat-update  (diff-chat-update:tc wire !<(chat-update q.cage.sign))