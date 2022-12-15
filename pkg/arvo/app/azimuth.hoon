gwbtc/urbit

commit 94ca595f7c636f077ea453e830cf72005e604ac1

wip

pkg/arvo/app/azimuth.hoon
@@ -4,7 +4,7 @@
     naive,
     dice,
     default-agent,
-    verb,
+/   verb,
     dbug
 ::  To update, run from dojo:
 ::    -azimuth-snap-state %default 'version-0'

.husky/post-checkout
@@ -1,0 +1,3 @@
+#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-checkout.\n"; exit 2; }
+git lfs post-checkout "$@"

.husky/post-commit
@@ -1,0 +1,3 @@
+#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-commit.\n"; exit 2; }
+git lfs post-commit "$@"

.husky/post-merge
@@ -1,0 +1,3 @@
+#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-merge.\n"; exit 2; }
+git lfs post-merge "$@"

.husky/pre-push
@@ -1,0 +1,3 @@
+#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\n"; exit 2; }
+git lfs pre-push "$@"

pkg/urbit/noun/manage.c
@@ -433,6 +433,7 @@ u3m_signal(u3_noun sig_l)
 u3_noun
 u3m_file(c3_c* pas_c)
 {
+  u3l_log("Loading file: %s\r\n", pas_c);
   struct stat buf_b;
   c3_i        fid_i = c3_open(pas_c, O_RDONLY, 0644);
   c3_w        fln_w, red_w;
@@ -443,6 +444,8 @@ u3m_file(c3_c* pas_c)
     return u3m_bail(c3__fail);
   }
   fln_w = buf_b.st_size;
+  //u3l_log("file size: %i\r\n", fln_w);
+  
   pad_y = c3_malloc(buf_b.st_size);
 
   red_w = read(fid_i, pad_y, fln_w);
@@ -455,6 +458,9 @@ u3m_file(c3_c* pas_c)
   else {
     u3_noun pad = u3i_bytes(fln_w, (c3_y *)pad_y);
     c3_free(pad_y);
+
+    //u3l_log("size of noun\r\n");
+    //u3m_p("size", u3dc("met", 3, u3k(pad)));
 
     return pad;
   }

pkg/urbit/vere/io/fore.c
@@ -78,9 +78,12 @@ _fore_inject(u3_auto* car_u, c3_c* pax_c)
 static void
 _fore_import(u3_auto* car_u, c3_c* pax_c)
 {
-  u3_noun arc = u3ke_cue(u3m_file(pax_c));
-  u3_noun imp = u3dt("cat", 3, u3i_string("#import_"), arc);
+  u3_noun fil = u3m_file(pax_c);
+  //u3m_p("imp file size", u3dc("met", 3, fil));
+  u3_noun imp = u3dt("cat", 3, u3i_string("#import_"), fil);
   u3_noun siz = u3r_met(3, imp);
+  u3m_p("imp cue size", siz);
+
   u3_noun dat = u3nt(u3_nul, siz, imp);
 
   u3_noun req = u3nt(c3n,

commit 7bf9950174bde8c19cfc44fbe1f574f279dde276

Fix build break typo in azimuth
Fixes typo introduced in 94ca595f7c636f077ea453e830cf72005e604ac1

pkg/arvo/app/azimuth.hoon
@@ -4,7 +4,7 @@
     naive,
     dice,
     default-agent,
-/   verb,
+    verb,
     dbug
 ::  To update, run from dojo:
 ::    -azimuth-snap-state %default 'version-0'